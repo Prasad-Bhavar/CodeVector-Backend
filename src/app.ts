e98a31bcfeae288aa38ddea9459554ecad52276d
prasad.bhavar <prasad.bhavar@intern.in>
2026-06-22 16:08:08 +0530

    cors upadated with frontend URL

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@ import routes from "./routes";
 import cors from "cors";
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://codevector-products-gr5e.onrender.com",
+    credentials: true,
+}));
 app.use(express.json());
 
 app.use("/api", routes);
